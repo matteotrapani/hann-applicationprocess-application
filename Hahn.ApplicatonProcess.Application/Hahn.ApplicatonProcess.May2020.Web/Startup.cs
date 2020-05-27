39c0c369122e2a98eae46a4933af44366b870b6f
Matteo Trapani <matteo.trapani92@gmail.com>
2020-05-27 10:47:12 +0200

    Fix SPA/swagger configuration in startup

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.May2020.Web/Startup.cs
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.May2020.Web/Startup.cs
@@ -21,87 +21,90 @@ using Serilog;
 
 namespace Hahn.ApplicatonProcess.May2020.Web
 {
-    public class Startup
+  public class Startup
+  {
+
+    public Startup(IConfiguration configuration)
     {
+      Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.ConfigureSwagger()
+          .ConfigureSwaggerExamples()
+          .ConfigureApplicantContext()
+          .ConfigureLogging(Configuration)
+          .ConfigureSerilog(Configuration)
+          .ConfigureAutomapper()
+          .ConfigureBusinessServices()
+          .ConfigureMvc()
+          .ConfigureFluentValidation()
+          ;
+
+      services.ConfigureCountriesApiHttpClient(Configuration.GetSection("CountriesApi"));
+      // In production, the Aurelia files will be served from this directory
+      services.AddSpaStaticFiles(configuration =>
+      {
+        configuration.RootPath = "wwwroot/dist";
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+      }
+
+      var logger =
+          new LoggerConfiguration()
+              .ReadFrom.Configuration(Configuration, "Serilog")
+              .CreateLogger();
+      loggerFactory.AddSerilog(logger);
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      if (!env.IsDevelopment())
+      {
+        app.UseSpaStaticFiles();
+      }
+
+      app.UseSerilogRequestLogging();
+
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute(
+          name: "default",
+          pattern: "{controller}/{action=Index}/{id?}");
+        // endpoints.MapControllerRoute(
+        //   name: "spa-fallback",
+        //   pattern: "{controller=Home}/{action=Index}");
+      });
+
+      app.UseSwagger()
+        .UseSwaggerUI(c =>
         {
-            Configuration = configuration;
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        });
+      app.UseSpa(spa => {
+        spa.Options.SourcePath = ".";
+
+        if (env.IsDevelopment())
+        {
+          spa.UseAureliaCliServer(); // Optional HMR (Hot Module Reload)
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.ConfigureSwagger()
-                .ConfigureSwaggerExamples()
-                .ConfigureApplicantContext()
-                .ConfigureLogging(Configuration)
-                .ConfigureSerilog(Configuration)
-                .ConfigureAutomapper()
-                .ConfigureBusinessServices()
-                .ConfigureMvc()
-                .ConfigureFluentValidation()
-                ;
-
-            services.ConfigureCountriesApiHttpClient(Configuration.GetSection("CountriesApi"));
-            // In production, the Aurelia files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-              configuration.RootPath = "wwwroot/dist";
-            });
+      });
     }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            var logger =
-                new LoggerConfiguration()
-                    .ReadFrom.Configuration(Configuration, "Serilog")
-                    .CreateLogger();
-            loggerFactory.AddSerilog(logger);
-
-            app.UseHttpsRedirection(); app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-            app.UseSpa(spa => {
-              spa.Options.SourcePath = ".";
-
-              if (env.IsDevelopment())
-              {
-                spa.UseAureliaCliServer(); // Optional HMR (Hot Module Reload)
-              }
-            });
-
-
-            app.UseSerilogRequestLogging();
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-              endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-              endpoints.MapControllerRoute(
-                name: "spa-fallback",
-                pattern: "{controller=Home}/{action=Index}");
-            });
-
-            app.UseSwagger()
-              .UseSwaggerUI(c =>
-              {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-              });
-    }
-    }
+  }
 }
